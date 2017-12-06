b5ca12b024affced129152e8a5acb89f1e6a1fc3
Christian Studer <cstuder@existenz.ch>
2017-12-06 21:25:26 +0100

    Removes mdl-date-picker.

--- www/application/views/html5.php
+++ www/application/views/html5.php
@@ -22,19 +22,7 @@ $title = isset($title) ? "Oberfeld Agenda &mdash; {$title}" : 'Oberfeld Agenda';
     <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Roboto:300,400,500,700" type="text/css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="<?php echo base_url('mdl/material.min.css'); ?>">
-    <link rel="stylesheet" href="<?php echo base_url('md-date-time-picker/css/mdDateTimePicker.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('css/agenda.css'); ?>">
-    <script src="<?php echo base_url('mdl/material.min.js'); ?>"></script>
-    <script src="<?php echo base_url('md-date-time-picker/js/moment.min.js'); ?>"></script>
-    <script src="<?php echo base_url('md-date-time-picker/js/lang/de.js'); ?>"></script>
-    <script src="<?php echo base_url('md-date-time-picker/js/scroll-into-view-if-needed.min.js'); ?>"></script>
-    <script src="<?php echo base_url('md-date-time-picker/js/draggabilly.pkgd.min.js'); ?>"></script>
-    <script src="<?php echo base_url('md-date-time-picker/js/mdDateTimePicker.js'); ?>"></script>
-    <script type="text/javascript">
-    (function() {
-      moment.locale('de');
-    }).call(this);
-    </script>
 </head>
 <body>
   <div class="mdl-layout mdl-js-layout">
