72f37c15f25d8fb9ff9357680caf58f6414a579f
Christian Studer <cstuder@existenz.ch>
2018-04-24 22:38:30 +0200

    Layout: Adds content

--- www/application/views/html5.php
+++ www/application/views/html5.php
@@ -26,17 +26,12 @@ $title = isset($title) ? "{$title} &mdash; Oberfeld Agenda" : 'Oberfeld Agenda';
     ?>
 </head>
 <body>
-<?php $this->load->view('parts/navbar', ['title' => $title]); ?>
-<!-- 
+  <?php $this->load->view('parts/navbar', ['title' => $title]); ?>
   
-  <main class="mdl-layout__content">
-    <div class="page-content">
-      <div class="mdl-grid">
-        <?php echo $content; ?>
-      </div>
-    </div>
-  </main>
-</div> -->
+  <div class="container-fluid">
+    <?php echo $content; ?>
+  </div>
+
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
