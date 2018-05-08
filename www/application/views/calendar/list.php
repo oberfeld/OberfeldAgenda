d3992afb2e09015b0a56ccec8c8c8c5d3c3ae0ec
Christian Studer <cstuder@existenz.ch>
2018-05-08 21:54:07 +0200

    Calendar list: Fixes image overlay covering buttons

--- www/application/views/calendar/list.php
+++ www/application/views/calendar/list.php
@@ -15,10 +15,8 @@
     <div class="col-sm-6 mt-3">
       <div class="card">
         <img class="card-img-top" src="<?php echo site_url("images/{$cal['image']}"); ?>" alt="Bild für <?php echo $cal['name']; ?>">
-        <div class="card-img-overlay">
-          <a href="<?php echo $cal['aboUrl']; ?>" title="Kalender abonnieren" class="float-right white"><span class="oi oi-cloud-download"></span></a>
-        </div>
         <div class="card-body">
+          <a href="<?php echo $cal['aboUrl']; ?>" title="Kalender abonnieren" class="float-right"><span class="oi oi-cloud-download"></span></a>
           <h5 class="card-title"><?php echo $cal['name']; ?></h5>
           <a href="<?php echo site_url("agenda/reservieren/{$cal['id']}"); ?>" class="btn btn-primary"><?php echo $buttonlabel; ?></a>
           <a href="<?php echo site_url("agenda/termine/{$cal['id']}"); ?>" class="btn btn-primary">Anzeigen</a>
