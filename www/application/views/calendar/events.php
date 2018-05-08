5136672c393663fc69feb1d8a60171e59eb23c50
Christian Studer <cstuder@existenz.ch>
2018-05-08 22:38:05 +0200

    Design: Use new part

--- www/application/views/calendar/events.php
+++ www/application/views/calendar/events.php
@@ -6,33 +6,20 @@
  * @author Christian Studer <cstuder@existenz.ch>
  */
 
-$calendar;
-
-if(isset($calendar['infos'])) {
-    $infocontent = $this->load->view("calendar-infos/{$calendar['infos']}", NULL, TRUE);
-?>
-<div class="row">
-  <div class="col-sm-12">
-    <div class="alert alert-info" role="alert">
-      <?php echo $infocontent; ?>
-    </div>
-  </div>
-</div>
-
-<?php
-}
+// Top: Show calendar informations
+$this->load->view('parts/calendar-infos', ['calendar' => $calendar]);
 
 // Left part: Cal card narrow, right part: iframe
 ?>
 <div class="row">
   <div class="col-sm-2">
     <?php
-    $this->load->view('parts/cal-card-narrow', [
-      'calendar' => $calendar,
-      'buttonlabel' => $calendar['buttonlabel'] ?? 'Reservieren',
-      'buttonurl' => site_url("agenda/reservieren/{$calendar['id']}"),
-    ]);
-?>
+      $this->load->view('parts/cal-card-narrow', [
+        'calendar' => $calendar,
+        'buttonlabel' => $calendar['buttonlabel'] ?? 'Reservieren',
+        'buttonurl' => site_url("agenda/reservieren/{$calendar['id']}"),
+      ]);
+    ?>
   </div>
 
   <div class="col-sm-10">
