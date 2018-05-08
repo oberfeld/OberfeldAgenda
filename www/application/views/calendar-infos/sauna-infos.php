c158784b785de812652eec64481c5479ce14cce4
Christian Studer <cstuder@existenz.ch>
2018-05-08 21:58:44 +0200

    Infotexts: More comments

--- www/application/views/calendar-infos/sauna-infos.php
+++ www/application/views/calendar-infos/sauna-infos.php
@@ -1,1 +1,10 @@
+<?php
+/**
+ * Saune information text
+ * 
+ * Usually pure HTML
+ * 
+ * @author Christian Studer <cstuder@existenz.ch>
+ */
+?>
 Alle Informationen zur Sauna findest du in den <a href="https://www.oberfeld.be/gemeinschaftsraeume/sauna/" target="_blank">Grünen Seiten</a>.
