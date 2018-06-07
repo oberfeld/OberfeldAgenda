c705a0b02c4fb357f26be8c9e83991b4c1e89d05
Christian Studer <cstuder@existenz.ch>
2018-06-07 22:52:41 +0200

    Edit form: Ported to Bootstrap, needs some more styling

--- www/application/helpers/MY_form_helper.php
+++ www/application/helpers/MY_form_helper.php
@@ -1,51 +1,64 @@
 <?php
 /**
- * MDL form helper
+ * Bootstrap form helper
  * 
  * @author Christian Studer <cstuder@existenz.ch>
  */
 
 /**
- * MDL Text Input Field
+ * Bootstrap Text Input Field
  *
  * @param string $label
  * @param string $name
  * @param string $value
  * @param string $type
+ * @param bool $required
  * @return string
  */
-function mdl_form_input($label, $name, $value, $type = 'text') {
-    $input = '<div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">';
-    $input .= "<label for=\"input__{$name}\" class=\"mdl-textfield__label\">{$label}</label>";
-    $input .= form_input($name, $value, ['id' => "input__{$name}", 'class' => "mdl-textfield__input", 'type' => $type]);
-    $input .= '</div>';
+function bootstrap_form_input($label, $name, $value, $type = 'text', $required = TRUE) {
+    $input = '<div class="form-group">';
+    $input .= "<label for=\"input_{$name}\">{$label}</label>";
+    $input .= "<input id=\"input_{$name}\" name=\"{$name}\" type=\"{$type}\" class=\"form-control here\"";
+    if($required) $input .= 'required="required"';
+    $input .='></div>';
     
     return $input;
 }
 
 /**
- * MDL Submit button
+ * Boostrap Submit button
  *
  * @param string $label
  * @return string
  */
-function mdl_form_submit($label) {
-    return form_submit($label, $label, ['class' => 'mdl-button mdl-button--accent mdl-button--raised mdl-js-button mdl-js-ripple-effect']);
+function bootstrap_form_submit($label) {
+    $submit = '<div class="form-group">';
+    $submit .= "<button name=\"submit\" type=\"submit\" class=\"btn btn-primary\">{$label}</button>";
+    $submit .= '</div>';
+
+    return $submit;
 }
 
 /**
- * MDL Dropdown
+ * Bootstrap Dropdown
  * 
  * @param string $label
  * @param string $name
  * @param array $options
  * @param string $selected
+ * @param bool $required
  * @return string
  */
-function mdl_form_dropdown($label, $name, array $options, $selected = NULL) {
-    $select = '<div class="mdl-selectfield mdl-js-selectfield">';
-    $select .= form_dropdown($name, $options, $selected, ['id' => "select__{$name}", 'class' => 'mdl-selectfield__select']);
-    // $select .= "<label class=\"mdl-selectfield__label\" for=\"select__{$name}\">{$label}</label>"; // TODO Doesn't work correctly, fix it later. (#5)
+function bootstrap_form_dropdown($label, $name, array $options, $selected = NULL, $required = FALSE) {
+    $select = '<div class="form-group">';
+    $select .= "<label for=\"input_{$name}\">{$label}</label>";
+    $attributes = [
+        'id' => "input_{$name}",
+        'class' => 'form-control',
+    ];
+    if($required) $attributes['required'] = 'required';
+    $select .= form_dropdown($name, $options, $selected, $attributes);
+
     $select .= '</div>';
 
     return $select;
--- www/application/views/calendar/eventform.php
+++ www/application/views/calendar/eventform.php
@@ -21,6 +21,65 @@ array_pop($timeoptions); // Remove 24:30.
 // Top: Show calendar informations
 $this->load->view('parts/calendar-infos', ['calendar' => $calendar]);
 
+/* 
+  <div class="form-group">
+    <label for="subject">Beschreibung</label> 
+    <input id="subject" name="subject" type="text" class="form-control here" required="required">
+  </div>
+  <div class="form-group">
+    <label for="fromdate">Von</label> 
+    <div class="input-group">
+      <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+      </div> 
+      <input id="fromdate" name="fromdate" type="text" class="form-control here" required="required">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="frommin">Uhrzeit</label> 
+    <select id="frommin" name="frommin" class="form-control" required="required">
+      <option value="0">00:00</option>
+      <option value="30">00:30</option>
+      <option value="480">08:00</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="todate">Bis</label> 
+    <div class="input-group">
+      <div class="input-group-addon">
+        <i class="fa fa-calendar"></i>
+      </div> 
+      <input id="todate" name="todate" type="text" class="form-control here" required="required">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="tomin">Uhrzeit</label> 
+    <select id="tomin" name="tomin" class="form-control" required="required">
+      <option value="rabbit">Rabbit</option>
+      <option value="duck">Duck</option>
+      <option value="fish">Fish</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="name">Dein Name</label> 
+    <input id="name" name="name" type="text" class="form-control here" required="required">
+  </div>
+  <div class="form-group">
+    <label for="email">Deine Email-Adresse</label> 
+    <input id="email" name="email" type="text" class="form-control here" required="required">
+  </div>
+  <div class="form-group">
+    <label for="tel">Deine Telefonnummer</label> 
+    <input id="tel" name="tel" type="text" class="form-control here" aria-describedby="telHelpBlock"> 
+    <span id="telHelpBlock" class="form-text text-muted">Optional, für Rückfragen</span>
+  </div> 
+  <div class="form-group">
+    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</form>
+
+*/
+
 // Left part: Cal card narrow, right part: event form
 ?>
 <div class="row">
@@ -40,20 +99,20 @@ $this->load->view('parts/calendar-infos', ['calendar' => $calendar]);
 <?php
   echo form_open("agenda/reservieren/{$calendar['id']}");
 
-  echo mdl_form_input('Beschreibung', 'subject', $event['subject'] ?? '') . br();
-  echo mdl_form_input('Von', 'fromdate', $event['fromdate'] ?? '', 'date') . ' ' . mdl_form_dropdown('Uhrzeit', 'frommin', $timeoptions, $event['frommin'] ?? '480') . br();
-  echo mdl_form_input('Bis', 'todate', $event['todate'] ?? '', 'date') . ' ' . mdl_form_dropdown('Uhrzeit', 'tomin', $timeoptions, $event['tomin'] ?? '540') . br();
+  echo bootstrap_form_input('Beschreibung', 'subject', $event['subject'] ?? '', 'text', TRUE) . br();
+  echo bootstrap_form_input('Von', 'fromdate', $event['fromdate'] ?? '', 'date', TRUE) . ' ' . bootstrap_form_dropdown('Uhrzeit', 'frommin', $timeoptions, $event['frommin'] ?? '480', TRUE) . br();
+  echo bootstrap_form_input('Bis', 'todate', $event['todate'] ?? '', 'date', TRUE) . ' ' . bootstrap_form_dropdown('Uhrzeit', 'tomin', $timeoptions, $event['tomin'] ?? '540', TRUE) . br();
 
   echo '<p>';
 
-  echo mdl_form_input('Dein Name', 'name', $event['name'] ?? '') . br();
-  echo mdl_form_input('Deine Email-Adresse', 'name', $event['email'] ?? '', 'email') . br();
-  echo mdl_form_input('Deine Telefonnummer', 'tel', $event['tel'] ?? '', 'tel') . br();
+  echo bootstrap_form_input('Dein Name', 'name', $event['name'] ?? '', 'text', TRUE) . br();
+  echo bootstrap_form_input('Deine Email-Adresse', 'name', $event['email'] ?? '', 'email', TRUE) . br();
+  echo bootstrap_form_input('Deine Telefonnummer', 'tel', $event['tel'] ?? '', 'tel', FALSE) . br();
 
   echo '<p>';
 
   echo form_hidden('eventid', $event['id'] ?? '');
-  echo mdl_form_submit($calendar['buttonlabel']);
+  echo bootstrap_form_submit($calendar['buttonlabel']);
   
   echo form_close();
 ?>
