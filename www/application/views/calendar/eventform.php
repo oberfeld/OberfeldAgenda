bd2a4cb8b61bea87b61fd65a829226d4d5ca3a60
Christian Studer <cstuder@existenz.ch>
2018-11-27 22:14:47 +0100

    Event: Removes old MD code

--- www/application/views/calendar/eventform.php
+++ www/application/views/calendar/eventform.php
@@ -21,65 +21,6 @@ array_pop($timeoptions); // Remove 24:30.
 // Top: Show calendar informations
 $this->load->view('parts/calendar-infos', ['calendar' => $calendar]);
 
-/* 
-  <div class="form-group">
-    <label for="subject">Beschreibung</label> 
-    <input id="subject" name="subject" type="text" class="form-control here" required="required">
-  </div>
-  <div class="form-group">
-    <label for="fromdate">Von</label> 
-    <div class="input-group">
-      <div class="input-group-addon">
-        <i class="fa fa-calendar"></i>
-      </div> 
-      <input id="fromdate" name="fromdate" type="text" class="form-control here" required="required">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="frommin">Uhrzeit</label> 
-    <select id="frommin" name="frommin" class="form-control" required="required">
-      <option value="0">00:00</option>
-      <option value="30">00:30</option>
-      <option value="480">08:00</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="todate">Bis</label> 
-    <div class="input-group">
-      <div class="input-group-addon">
-        <i class="fa fa-calendar"></i>
-      </div> 
-      <input id="todate" name="todate" type="text" class="form-control here" required="required">
-    </div>
-  </div>
-  <div class="form-group">
-    <label for="tomin">Uhrzeit</label> 
-    <select id="tomin" name="tomin" class="form-control" required="required">
-      <option value="rabbit">Rabbit</option>
-      <option value="duck">Duck</option>
-      <option value="fish">Fish</option>
-    </select>
-  </div>
-  <div class="form-group">
-    <label for="name">Dein Name</label> 
-    <input id="name" name="name" type="text" class="form-control here" required="required">
-  </div>
-  <div class="form-group">
-    <label for="email">Deine Email-Adresse</label> 
-    <input id="email" name="email" type="text" class="form-control here" required="required">
-  </div>
-  <div class="form-group">
-    <label for="tel">Deine Telefonnummer</label> 
-    <input id="tel" name="tel" type="text" class="form-control here" aria-describedby="telHelpBlock"> 
-    <span id="telHelpBlock" class="form-text text-muted">Optional, für Rückfragen</span>
-  </div> 
-  <div class="form-group">
-    <button name="submit" type="submit" class="btn btn-primary">Submit</button>
-  </div>
-</form>
-
-*/
-
 // Left part: Cal card narrow, right part: event form
 ?>
 <div class="row">
